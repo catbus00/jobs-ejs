720e12cd5162f572c1ed8bd3425682b0336eae03
catbus00 <catmousekill@gmail.com>
2024-01-22 15:53:40 +0700

    Make a separate validation for more reusability per @jcheng1022 's feedback

--- routes/secretWord.js
+++ routes/secretWord.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
   if (!req.session.secretWord) {
     req.session.secretWord = 'syzygy';
@@ -10,8 +9,16 @@ router.get('/', (req, res) => {
   res.render('secretWord', { secretWord: req.session.secretWord });
 });
 
+const validate = (secretWord) => {
+  if (secretWord.toUpperCase()[0] === 'P') {
+    return false;
+  }
+  return true;
+};
+
 router.post('/', (req, res) => {
-  if (req.body.secretWord.toUpperCase()[0] == 'P') {
+  const isValid = validate(req.body.secretWord);
+  if (!isValid) {
     req.flash('error', "That word won't work!");
     req.flash('error', "You can't use words that start with p.");
   } else {
